4e0de47ded4f919144a86c913f962e51ba27cbe1
Zeeshan Ayaz <zeeshanayaz1@gmail.com>
2020-09-28 13:51:19 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,17 +6,18 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {/* Edit <code>src/App.js</code> and save to reload. */}
+          Hello World
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.facebook.com/zeeshanayaz0"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Muhammad Zeeshan Ayaz
         </a>
       </header>
     </div>
