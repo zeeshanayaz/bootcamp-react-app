35c8c490462b6fc6bec249f343c1907cb182362f
Zeeshan Ayaz <zeeshanayaz1@gmail.com>
2020-09-28 16:22:28 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
           {/* Edit <code>src/App.js</code> and save to reload. */}
-          Hello to React World
+          Hello to React World 2020
         </p>
         <a
           className="App-link"
